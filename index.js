4a09c1b7b064cd9d27e34d41562ef7c2501d2a73
Daisuke Fujiwara <daisuke.fujiwara@gmail.com>
2019-04-07 15:18:09 -0700

    Fetch metadata of the randomly selected file (#9)
    this will be used to determine how to treat the file. (photo vs video)

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const storage = require('./storage')
 
 const run = async () => {
   try {
-    const file = await storage.selectRandomFile()
+    const { file } = await storage.selectRandomFile()
     const photoData = await photos.getPhoto(file)
     await email.send(photoData, config.recipients, config.emailUserName, config.password)
   } catch (error) {
