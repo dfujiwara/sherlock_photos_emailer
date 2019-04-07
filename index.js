1cf0954ad0f98de0d42f0256f736ad271f1f598f
Daisuke Fujiwara <daisuke.fujiwara@gmail.com>
2019-04-07 11:30:39 -0700

    Separate out photos related logic in to photos module (#5)

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const config = require('./config')
 const { Storage } = require('@google-cloud/storage')
-const { ExifImage } = require('exif')
 const redis = require('redis')
 const { promisify } = require('util')
 const log = require('./log')
 const email = require('./email')
+const photos = require('./photos')
 
 const storage = new Storage({
   projectId: config.projectId
@@ -38,42 +38,10 @@ const selectRandomPhoto = async () => {
   return randomPhotoFile
 }
 
-const getPhoto = async (file) => {
-  const fileURL = './photo.jpeg'
-  await storage
-    .bucket(config.storageBucketName)
-    .file(file.name)
-    .download({ destination: fileURL })
-  return fileURL
-}
-
-const getPhotoData = (photoURL) => {
-  return new Promise((resolve, reject) => {
-    ExifImage({ image: photoURL }, (err, exif) => {
-      if (err !== null) {
-        log.error(err)
-        resolve({
-          photoURL,
-          subject: `Sherlock photo of the day`
-        })
-      } else {
-        // Parse the date portion from the exif date time and
-        const split = exif.exif.DateTimeOriginal.split(' ')
-        const photoDate = split[0].replace(/:/g, '-')
-        resolve({
-          photoURL,
-          subject: `Sherlock photo of the day from ${photoDate}`
-        })
-      }
-    })
-  })
-}
-
 const run = async () => {
   try {
     const file = await selectRandomPhoto()
-    const photoURL = await getPhoto(file)
-    const photoData = await getPhotoData(photoURL)
+    const photoData = await photos.getPhoto(file)
     await email.send(photoData, config.recipients, config.emailUserName, config.password)
   } catch (error) {
     log.error(error)
