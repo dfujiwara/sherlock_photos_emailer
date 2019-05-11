d00329c35d736692828feb8170496edc44ed35f5
Daisuke Fujiwara <daisuke.fujiwara@gmail.com>
2019-05-11 16:33:35 -0700

    Attach video thumbnail to the email (#19)

--- src/videos.js
+++ src/videos.js
@@ -1,7 +1,14 @@
-const getVideo = (file) => {
+const storage = require('./storage')
+const genThumbnail = require('simple-thumbnail')
+
+const getVideo = async (file) => {
+  const fileURL = './photo.mov'
+  const thumbnailURL = './video_thumbnail.png'
+  await storage.download(file, fileURL)
+  await genThumbnail(fileURL, thumbnailURL, '200x?')
   return new Promise((resolve, reject) => {
     resolve({
-      files: [],
+      files: [thumbnailURL],
       subject: `Sherlock video of the day`,
       text: `Sherlock's best time at ${file.metadata.mediaLink}`
     })
